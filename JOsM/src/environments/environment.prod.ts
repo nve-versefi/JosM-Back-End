abbe1838b3233d00d9ade7b823de5e6d3e2ecca3
nve-versefi <n.vaneerde@versefi.co.uk>
2024-02-25 22:09:52 +0000

    Cleaned Up Unnecesary Files

--- JOsM/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: true,
-    apiUrl: 'http://www.orquestajosm.com/api', // URL del servidor de desarrollo
-  };
--- JOsM/src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:4200/api', // URL del servidor de desarrollo
-  };
