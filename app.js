49f16d2b05be6cf8f5e9239f3bb5b7613b3db726
nve-versefi <n.vaneerde@versefi.co.uk>
2024-02-27 13:56:15 +0000

    Updated CORS Again

--- app.js
+++ app.js
@@ -8,8 +8,13 @@ const path = require('path');
 
 const app = express();
 
-app.use(cors({
-  origin: process.env.CORS_ORIGIN
+app.use(cors((req, callback) => {
+  console.log("CORS Origin: ", process.env.CORS_ORIGIN); // Debug log
+  var corsOptions = {
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  };
+  callback(null, corsOptions);
 }));
 
 
